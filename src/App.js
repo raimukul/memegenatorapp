ba5a380b9ea09c8afcc4498be2ee114ac46b7cea
btrchain <47410631+raimukul@users.noreply.github.com>
2022-05-22 14:17:08 +0530

    ccc

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from "react"
 import Header from "./Components/Header"
 import MemeGenerator from "./Components/MemeGen"
 import './App.css'
+import { Container } from "@material-ui/core"
 
 function App() {
     return (
+        <div>
+        <Container>
         <div className="app-container" data-theme="dark">
             <Header />
             <MemeGenerator />
             
+        </div>
+        </Container>
         </div>
     )
 }
